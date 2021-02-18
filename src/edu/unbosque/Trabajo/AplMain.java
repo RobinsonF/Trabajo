cd61886ab7601ae8ef833526720d7b7a9858b6e9
RobinsonF <rgutierrezs@unbosque.edu.co>
2021-02-17 22:22:15 -0500

    Listo

--- src/edu/unbosque/Trabajo/AplMain.java
+++ src/edu/unbosque/Trabajo/AplMain.java
@@ -1,7 +1,11 @@
 package edu.unbosque.Trabajo;
 
+import java.util.Date;
+
 public class AplMain {
     public static void main(String[] args){
+        Date date = new Date();
         System.out.println("Hola mundo, estamos en el curso de programación");
+        System.out.println("La fecha de hoy: " + date.toString());
     }
 }
