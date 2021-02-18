109fed2e1943c12f3a94629d1b1f5386ea5c7e78
RobinsonF <73041810+RobinsonF@users.noreply.github.com>
2021-02-17 22:04:46 -0500

    Expandiendo el código

--- src/edu/unbosque/Trabajo/AplMain.java
+++ src/edu/unbosque/Trabajo/AplMain.java
@@ -2,6 +2,6 @@ package edu.unbosque.Trabajo;
 
 public class AplMain {
     public static void main(String[] args){
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo, estamos en el curso de programación");
     }
 }
